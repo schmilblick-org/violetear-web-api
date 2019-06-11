fbc86e83c74e8c4aaa7796c6110ba08ca2dd9c2f
Leo Le Bouter <lle-bout@zaclys.net>
2019-06-11 22:26:04 +0200

    Add Procfile and take port from env PORT

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use actix_web::{middleware, App, HttpServer};
+use std::env;
 
 mod routes;
 
 fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
     env_logger::init();
+
+    let port = env::var("PORT");
 
     let args = clap::App::new("Violetear Web API")
         .arg(
@@ -18,8 +21,7 @@ fn main() -> std::io::Result<()> {
             clap::Arg::with_name("listen-port")
                 .short("p")
                 .takes_value(true)
-                .number_of_values(1)
-                .default_value("5000"),
+                .number_of_values(1),
         )
         .get_matches();
 
@@ -33,8 +35,11 @@ fn main() -> std::io::Result<()> {
     .bind((
         args.value_of("listen-address")
             .expect("listen-address argument missing"),
-        args.value_of("listen-port")
-            .expect("listen-port argument missing")
+        (&port.unwrap_or(
+            args.value_of("listen-port")
+                .expect("listen-port argument missing")
+                .to_owned(),
+        ))
             .parse()
             .expect("listen-port argument invalid"),
     ))?
