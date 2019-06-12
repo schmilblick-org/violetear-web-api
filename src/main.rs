fda5e2dd507d8820768d498608834544ab6fda30
Leo Le Bouter <lle-bout@zaclys.net>
2019-06-12 09:08:39 +0200

    Implement /register route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{middleware, App, HttpServer};
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::NO_PARAMS;
 use std::env;
 
 mod routes;
@@ -23,8 +25,22 @@ fn main() -> std::io::Result<()> {
         )
         .get_matches();
 
-    HttpServer::new(|| {
+    let manager = SqliteConnectionManager::file("web-api.db");
+    let pool = r2d2::Pool::new(manager).unwrap();
+
+    let conn = pool.get().unwrap();
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, rank INTEGER)",
+        NO_PARAMS,
+    )
+    .unwrap();
+
+    conn.execute("CREATE TABLE IF NOT EXISTS tokens (user_id INTEGER, token TEXT, created_when DATETIME DEFAULT CURRENT_TIMESTAMP)", NO_PARAMS).unwrap();
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .wrap(middleware::DefaultHeaders::new())
             .wrap(middleware::Compress::default())
             .wrap(middleware::Logger::default())
@@ -36,8 +52,8 @@ fn main() -> std::io::Result<()> {
         (&env::var("PORT").unwrap_or_else(|_| {
             args.value_of("listen-port")
                 .expect("listen-port argument missing")
-                .to_owned()}
-        ))
+                .to_owned()
+        }))
             .parse()
             .expect("listen-port argument invalid"),
     ))?
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,59 @@
-use actix_web::get;
+use actix_web::{get, post, web, HttpResponse};
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
 
+use rusqlite::{ToSql, NO_PARAMS};
+use serde_derive::{Deserialize, Serialize};
 #[get("/")]
 fn index() -> &'static str {
     "You have reached a Violetear Web API."
+}
+
+#[derive(Serialize, Deserialize)]
+struct Register {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct RegisterResponse {
+    token: Option<String>,
+}
+
+#[post("/register")]
+fn register(
+    db: web::Data<Pool<SqliteConnectionManager>>,
+    register: web::Json<Register>,
+) -> HttpResponse {
+    let conn = db.get().unwrap();
+
+    if conn
+        .execute(
+            "SELECT username FROM users WHERE username = $1",
+            &[&register.username],
+        )
+        .unwrap()
+        > 0
+    {
+        HttpResponse::Conflict().json(RegisterResponse { token: None })
+    } else {
+
+        conn.execute("BEGIN", NO_PARAMS).unwrap();
+        conn.execute(
+            "INSERT INTO users (username, password) VALUES ($1, $2)",
+            &[&register.username, &register.password],
+        )
+        .unwrap();
+
+        let token = uuid::Uuid::new_v4().to_simple().to_string().to_lowercase();
+
+        conn.execute(
+            "INSERT INTO tokens (user_id, token) VALUES ($1, $2)",
+            &[&conn.last_insert_rowid() as &ToSql, &token],
+        )
+        .unwrap();
+        conn.execute("COMMIT", NO_PARAMS).unwrap();
+
+        HttpResponse::Ok().json(RegisterResponse { token: Some(token) })
+    }
 }
