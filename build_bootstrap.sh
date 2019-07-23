6faab27f7fb2984dad7aece5b51c949e14eb197e
Leo Le Bouter <lle-bout@zaclys.net>
2019-07-23 23:45:18 +0000

    Revert "Update build_bootstrap.sh"
    This reverts commit e5d413f820b9e2672a0749c6a03e9f0080781856

--- build_bootstrap.sh
+++ build_bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
-#if ! docker info &>/dev/null; then
-#  if [ -z "$DOCKER_HOST" -a "$KUBERNETES_PORT" ]; then
-#    export DOCKER_HOST='tcp://localhost:2375'
-#  fi
-#fi
+if ! docker info &>/dev/null; then
+  if [ -z "$DOCKER_HOST" -a "$KUBERNETES_PORT" ]; then
+    export DOCKER_HOST='tcp://localhost:2375'
+  fi
+fi
 
 if [[ -n "$CI_REGISTRY_USER" ]]; then
   echo "Logging to GitLab Container Registry with CI credentials..."
