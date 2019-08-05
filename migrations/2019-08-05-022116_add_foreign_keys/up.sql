ab4448817a76ab149590098ab603733325bc13ac
Leo Le Bouter <lle-bout@zaclys.net>
2019-08-05 06:13:29 +0200

    Fix foreign keys

--- migrations/2019-08-05-022116_add_foreign_keys/up.sql
+++ migrations/2019-08-05-022116_add_foreign_keys/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
-ALTER TABLE tokens ADD CONSTRAINT user_id FOREIGN KEY (id) REFERENCES users(id);
-ALTER TABLE tasks ADD CONSTRAINT report_id FOREIGN KEY (id) REFERENCES reports(id);
-ALTER TABLE tasks ADD CONSTRAINT profile_id FOREIGN KEY (id) REFERENCES profiles(id);
-ALTER TABLE reports ADD CONSTRAINT user_id FOREIGN KEY (id) REFERENCES users(id);
-ALTER TABLE worker_capabilities ADD CONSTRAINT worker_id FOREIGN KEY (id) REFERENCES workers(id);
-ALTER TABLE worker_capabilities ADD CONSTRAINT profile_id FOREIGN KEY (id) REFERENCES profiles(id);
+ALTER TABLE tokens ADD CONSTRAINT user_id_foreign FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE tasks ADD CONSTRAINT report_id_foreign FOREIGN KEY (report_id) REFERENCES reports(id);
+ALTER TABLE tasks ADD CONSTRAINT profile_id_foreign FOREIGN KEY (profile_id) REFERENCES profiles(id);
+ALTER TABLE reports ADD CONSTRAINT user_id_foreign FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE worker_capabilities ADD CONSTRAINT worker_id_foreign FOREIGN KEY (worker_id) REFERENCES workers(id);
+ALTER TABLE worker_capabilities ADD CONSTRAINT profile_id_foreign FOREIGN KEY (profile_id) REFERENCES profiles(id);
